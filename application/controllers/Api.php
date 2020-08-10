434b43a7872841f321382f4f2eab5eccafd6d676
kmj1dev <kmj1dev@gmail.com>
2020-08-10 11:50:14 +0700

    initial commit codeigniter rest server

---
+++ application/controllers/Api.php
@@ -0,0 +1,53 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require_once APPPATH. 'core/MY_RestController.php';
+
+class Api extends MY_RestController
+{
+	
+	public function index_get()
+	{
+		$users = [
+            ['id' => 0, 'name' => 'John', 'email' => 'john@example.com'],
+            ['id' => 1, 'name' => 'Jim', 'email' => 'jim@example.com'],
+        ];
+
+        $id = $this->get( 'id' );
+
+        if ( $id === null )
+        {
+            // Check if the users data store contains users
+            if ( $users )
+            {
+                // Set the response and exit
+                $this->response( $users, 200 );
+            }
+            else
+            {
+                // Set the response and exit
+                $this->response( [
+                    'status' => false,
+                    'message' => 'No users were found'
+                ], 404 );
+            }
+        }
+        else
+        {
+            if ( array_key_exists( $id, $users ) )
+            {
+                $this->response( $users[$id], 200 );
+            }
+            else
+            {
+                $this->response( [
+                    'status' => false,
+                    'message' => 'No such user found'
+                ], 404 );
+            }
+        }
+	}
+}
+
+/* End of file Api.php */
+/* Location: ./application/controllers/Api.php */
---
+++ application/core/MY_RestController.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+use chriskacerguis\RestServer\RestController;
+
+class MY_RestController extends RestController
+{
+	function __construct()
+	{
+		parent::__construct();
+	}
+}
+
+/* End of file MY_RestController.php */
+/* Location: ./application/core/MY_RestController.php */
